d2b602151de5e65e378cde223d87998f1773f073
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-12 22:57:58 -0500

    Adding dotenv

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const app = require('express')()
 
 app.get('/', (req, res, next) => {
    res.send("<h1>Hello</h1>")
 })
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
    console.log("Hey there")
 })
