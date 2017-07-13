4a698a76771e95dc1aaeae08674ea363c1917153
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-12 22:52:28 -0500

    Adding process.env for production

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = require('express')()
 app.get('/', (req, res, next) => {
    res.send("<h1>Hello</h1>")
 })
-app.listen(3000, () => {
+app.listen(process.env.port || 3000, () => {
    console.log("Hey there")
 })
