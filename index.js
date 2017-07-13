369d907f10b1786a8bfbc1a59ac1d1bfad38b529
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-12 22:55:03 -0500

    Correcting environment variable for port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = require('express')()
 app.get('/', (req, res, next) => {
    res.send("<h1>Hello</h1>")
 })
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
    console.log("Hey there")
 })
