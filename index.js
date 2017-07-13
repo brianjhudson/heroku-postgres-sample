fd0baa82bbacb8247eb3f9b253aa45068fa1f09c
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-12 23:07:54 -0500

    Added massive

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require('dotenv').config()
 const app = require('express')()
+const massive = require('massive')
 
+massive(process.env.DATABASE_URL)
+.then(db => {
+   console.log(db)
+   app.set('db', db)
+})
 app.get('/', (req, res, next) => {
-   res.send("<h1>Hello</h1>")
+   req.app.get('db').city.find({'population >': 3000000}, {order: 'population desc', limit: 1})
+   .then(results => {
+      res.send("<h1>Hello</h1><h2>Most Populous City From Database</h2><p>" + results[0].name + "</p>")
+   })
 })
 app.listen(process.env.PORT, () => {
    console.log("Hey there")
